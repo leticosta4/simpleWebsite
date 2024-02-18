10efcf0166388fe266c7a6ac3008157dd410281f
leticosta4 <leticiacostaoa@gmail.com>
2024-02-18 19:33:52 -0300

    final additions acordding to the course - better improve it

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//comportamentos e interatividade
+'use strict' //modo estrito: reduz erros silenciosos, melhora o desempenho, fornece mais avisos e menos recursos não seguros.
+
+const switcher = document.querySelector('.btn') //obter a referencia do botao, com o ponto p selecionar elementos pela classe CSS.
+
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme') //método toggle para alternar o elemento para a classe dark-theme
+    //iterando pela lsita de classes p verificar cada uma delas:
+    var classes = document.body.classList; 
+    var isDarkTheme = classes.contains("dark-theme");
+
+    if(isDarkTheme){
+        this.textContent = "Light theme"; 
+    } else {
+        this.textContent = "Dark theme";
+    }
+    console.log("current class name: " + classes);
+});
+
+
